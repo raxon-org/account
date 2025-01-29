be3b320c081dd740281c692c909e802ddf6af58f
remco van der Velde <remco.pc@outlook.com>
2025-01-29 18:29:21 +0100

    task package

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -833,11 +833,12 @@ trait Main
             $object = $this->object();
             Core::interactive();
             while(true){
+                echo Cli::error('No valid email found...') . PHP_EOL;
                 $email = Cli::read(Cli::INPUT, 'Email: ');
                 if(filter_var($email, FILTER_VALIDATE_EMAIL)){
                     break;
                 }
-                echo Cli::error('No valid email found...') . PHP_EOL;
+
                 flush();
             }
             while(true){
