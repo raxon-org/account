1fb830cec8a7d13ad9819d9b5229338b46e767b4
remco van der Velde <remco.pc@outlook.com>
2025-03-22 22:12:34 +0100

    sqlite

--- src/Package/Trait/User.php
+++ src/Package/Trait/User.php
@@ -130,26 +130,31 @@ trait User
         $object = $this->object();
         $url = $object->config('project.dir.vendor') . 'raxon/account/Data/Role.Anonymous.json';
         $data = $object->data_read($url);
+        $permission = [];
         if($data){
             foreach($data->get('permission') as $permission){
                 $node = new Node($object);
-                $result = $node->record('Account.Permission', $node->role_system(), [
+                $response = $node->record('Account.Permission', $node->role_system(), [
                     'filter' => [
                         'name' => $permission->name
                     ]
                 ]);
-                ddd($result);
-                if($result === null){
-                    $request = (object) [
-                        'name' => $permission,
-                    ];
-                    $entity = 'Permission';
-                    $config = Database::config($object);
-                    $connection = Database::entity_manager($object, $config);
-                    $permission = Entity::create($object, $connection, $node->role_system(), $entity, $request, $error);
-                    if($permission === null && $error !== null){
-                        d($error);
-                    }
+                if(
+                    array_key_exists('node', $response) &&
+                    property_exists($response['node'], 'uuid')
+                ){
+                    $permission[] = $response['node']->uuid;
+                }
+                else{
+                    //create permission
+                    $response = $node->create(
+                        'Account.Permission',
+                        $node->role_system(),
+                        [
+                            'name' => $permission->name
+                        ]
+                    );
+                    ddd($response);
                 }
             }
         }
