69374bb7ddbe626b35c1a88eccbeb8e6545fdbc4
remco van der Velde <remco.pc@outlook.com>
2025-03-23 17:45:34 +0100

    sqlite

--- src/Package/Trait/User.php
+++ src/Package/Trait/User.php
@@ -242,6 +242,7 @@ trait User
             ]
         ]);
         if(
+            is_array($response) &&
             array_key_exists('node', $response) &&
             property_exists($response['node'], 'uuid')
         ){
