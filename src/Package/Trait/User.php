9d6a1bf4102f1e2f9af832e00e8e27ca21f190ba
remco van der Velde <remco.pc@outlook.com>
2025-04-11 08:18:08 +0200

    test 001 user login json

--- src/Package/Trait/User.php
+++ src/Package/Trait/User.php
@@ -130,7 +130,6 @@ trait User
         $object = $this->object();
         $url = $object->config('project.dir.vendor') . 'raxon/account/Data/Role.Anonymous.json';
         $data = $object->data_read($url);
-        ddd($data);
         $permission_array = [];
         if($data){
             foreach($data->get('permission') as $permission){
