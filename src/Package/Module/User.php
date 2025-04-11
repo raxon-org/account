fc7ee0d88861150dc2f6308bf88137647441c5b2
remco van der Velde <remco.pc@outlook.com>
2025-04-11 17:02:21 +0200

    test 001 user login json

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -118,6 +118,7 @@ class User
             ]
         );
         $role = $response['list'][0];
+        ddd($role);
         $entity = 'User';
         $function = 'login';
         $expose = \Raxon\Doctrine\Module\Entity::expose_get(
