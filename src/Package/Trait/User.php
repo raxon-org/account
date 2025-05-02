6f8bfe62791814dcf6589e80bfe94fe4e1a31501
remco van der Velde <remco.pc@outlook.com>
2025-05-02 18:22:28 +0200

    implementing new parser

--- src/Package/Trait/User.php
+++ src/Package/Trait/User.php
@@ -124,6 +124,8 @@ trait User
         $connection->manager = Database::entity_manager($object, $config, $connection);
         $validate_url = Entity::get_validate_url($object, $entity);
         $validation = Entity::get_validation($object, $validate_url, $entity . '.patch');
+        d($validate_url);
+        d($entity. '.patch');
         ddd($validation);
         $object->config('doctrine.entity.manager', $connection->manager);
         if(File::exist($validate_url)) {
