55b311fa7b2dfa5a6895339a25bbcc5c2b531ccc
remco van der Velde <remco.pc@outlook.com>
2025-05-02 18:57:27 +0200

    implementing new parser

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -54,10 +54,6 @@ class User
                 'email' => $input->email,
                 'isActive' => 1
             ]);
-            $node->setPassword(password_hash($input->password, PASSWORD_BCRYPT, ['cost' => 13]));
-            $connection->manager->persist($node);
-            $connection->manager->flush();
-            ddd($node);
             if($node) {
                 $password = $input->password;
                 $verify = password_verify($password, $node->getPassword());
