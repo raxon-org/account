0e6d5bc481621bc745b69f5773d55af899e3f392
remco van der Velde <remco.pc@outlook.com>
2025-05-02 23:20:00 +0200

    implementing new parser

--- src/Package/Data/Extension.sql
+++ src/Package/Data/Extension.sql
@@ -14,29 +14,29 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 -- Dumping data for table p343606_wat_prod.extension: ~22 rows (approximately)
-INSERT INTO `extension` (`id`, `name`) VALUES
-	(22, '3'),
-	(7, 'html'),
-	(21, 'ini'),
-	(13, 'JPG'),
-	(9, 'js'),
-	(3, 'json'),
-	(20, 'log'),
-	(4, 'md'),
-	(1, 'mp3'),
-	(2, 'mp4'),
-	(15, 'ogg'),
-	(5, 'php'),
-	(19, 'png'),
-	(6, 'tpl'),
-	(10, 'txt'),
-	(11, 'vob'),
-	(16, 'wav'),
-	(17, 'webm'),
-	(14, 'wma'),
-	(12, 'wmv'),
-	(8, 'xml'),
-	(18, 'ytdl');
+INSERT INTO `extension` (`id`, `uuid`, `name`) VALUES
+	(22, "7beff074-4e9f-4d05-a047-14724ff418e3", '3'),
+	(7, "1cfcb578-9f3a-4e3f-a6d0-267a8b159552", 'html'),
+	(21, "eb15a711-e2b9-45c2-9504-f29c7f4f6352", 'ini'),
+	(13, "bad913fb-5b95-4038-8c42-54780833e0b8", 'JPG'),
+	(9, "a870ee98-bea6-47c9-ac41-c8fef6d680a3", 'js'),
+	(3, "50b0d9c9-7af2-4caf-afdf-6ea30f26794e", 'json'),
+	(20, "8464ab8e-3ad4-45cf-abf6-fcfeec1e35ec", 'log'),
+	(4, "4ecd60aa-7906-444e-b05a-274ac15b37a2", 'md'),
+	(1, "46225e43-c8a3-4ce3-8be5-e52ba0c350d5", 'mp3'),
+	(2, "3d080c7b-75b1-4718-a053-b537c5bbe2f0", 'mp4'),
+	(15, "30d1de6e-cbbc-4296-b0ab-605c2246df81", 'ogg'),
+	(5, "ade25b44-463a-44d6-af4d-368afbf5c700", 'php'),
+	(19, "33bee2ca-7879-4c42-a315-ae9cb21e1426", 'png'),
+	(6, "a67a3bef-1ff4-46c0-9505-d59255a47c18", 'tpl'),
+	(10, "d3dff7b7-fc66-49bf-bb48-a090407cdd0b", 'txt'),
+	(11, "f5a98d05-1cda-42a2-a43a-3102bb9391d2", 'vob'),
+	(16, "f1b46a7e-de0f-4cd0-8225-2d7a400fa4bd", 'wav'),
+	(17, "a3190846-c4f4-4207-9675-c64bff865049", 'webm'),
+	(14, "ba8983b0-a5d3-48c5-8bf8-684399dc1ce0", 'wma'),
+	(12, "be05027e-b7a6-4834-9102-4a8407b86f11", 'wmv'),
+	(8, "9535a099-28bc-408d-a255-1cf6877545cd", 'xml'),
+	(18, "26701895-67ad-456a-ba80-3f1ef5242097", 'ytdl');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
