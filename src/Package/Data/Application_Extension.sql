10b0670f7ac3383a84423257fb585c5e202a07f4
remco van der Velde <remco.pc@outlook.com>
2025-05-02 23:13:48 +0200

    implementing new parser

---
+++ src/Package/Data/Application_Extension.sql
@@ -0,0 +1,51 @@
+-- --------------------------------------------------------
+-- Host:                         91.184.19.106
+-- Server version:               10.6.20-MariaDB-cll-lve - MariaDB Server
+-- Server OS:                    Linux
+-- HeidiSQL Version:             12.6.0.6765
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table p343606_wat_prod.application_extension: ~22 rows (approximately)
+INSERT INTO `application_extension` (`application_id`, `extension_id`) VALUES
+	(1, 1),
+	(1, 2),
+	(1, 11),
+	(1, 12),
+	(1, 14),
+	(1, 15),
+	(1, 16),
+	(1, 17),
+	(2, 3),
+	(2, 4),
+	(2, 5),
+	(2, 6),
+	(2, 7),
+	(2, 8),
+	(2, 9),
+	(2, 10),
+	(2, 18),
+	(2, 20),
+	(2, 21),
+	(2, 22),
+	(6, 13),
+	(6, 19),
+	(7, 2),
+	(7, 17),
+	(8, 1),
+	(8, 15),
+	(8, 16);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ src/Package/Data/Extension.sql
@@ -0,0 +1,45 @@
+-- --------------------------------------------------------
+-- Host:                         91.184.19.106
+-- Server version:               10.6.20-MariaDB-cll-lve - MariaDB Server
+-- Server OS:                    Linux
+-- HeidiSQL Version:             12.6.0.6765
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Dumping data for table p343606_wat_prod.extension: ~22 rows (approximately)
+INSERT INTO `extension` (`id`, `name`) VALUES
+	(22, '3'),
+	(7, 'html'),
+	(21, 'ini'),
+	(13, 'JPG'),
+	(9, 'js'),
+	(3, 'json'),
+	(20, 'log'),
+	(4, 'md'),
+	(1, 'mp3'),
+	(2, 'mp4'),
+	(15, 'ogg'),
+	(5, 'php'),
+	(19, 'png'),
+	(6, 'tpl'),
+	(10, 'txt'),
+	(11, 'vob'),
+	(16, 'wav'),
+	(17, 'webm'),
+	(14, 'wma'),
+	(12, 'wmv'),
+	(8, 'xml'),
+	(18, 'ytdl');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
