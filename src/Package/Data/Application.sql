efba2f3df726e3e86599aedbdce539f4d87d2247
remco van der Velde <remco.pc@outlook.com>
2025-05-02 22:42:21 +0200

    implementing new parser

--- src/Package/Data/Application.sql
+++ src/Package/Data/Application.sql
@@ -1,6 +1,2 @@
 INSERT INTO `application` (`id`, `uuid`, `name`, `url`, `icon_url`) VALUES
-    (1, '11fb658d-eef6-421f-867c-81ca7d2cd18d', 'Audio Player', '{{route.get(\'application-audio-player\')}}', '{{route.get(\'application-audio-player-icon\')}}'),
-    (2, '7dfd23bc-31af-47ba-ad82-74fbfc807f84', 'Ace Editor', '{{route.get(\'application-ace-editor\')}}', '{{route.get(\'application-ace-editor-icon\')}}'),
-    (6, '854febe0-c23b-4afb-8b18-a45ba2d562c4', 'Photo Viewer', '{{route.get(\'application-photo-viewer\')}}', '{{route.get(\'application-photo-viewer-icon\')}}'),
-    (7, '96749beb-d808-4a95-be56-b66f40ccabed', 'Video Player', '{{route.get(\'application-video-player\')}}', '{{route.get(\'application-video-player-icon\')}}'),
-    (8, 'd03d57e8-d39d-40bb-89b7-b5a81343d1bd', 'Music Player', '{{route.get(\'application-music-player\')}}', '{{route.get(\'application-audio-player-icon\')}}');
+    (1, '11fb658d-eef6-421f-867c-81ca7d2cd18d', 'Audio Player', '{{route.get(\'application-audio-player\')}}', '{{route.get(\'application-audio-player-icon\')}}');
