d16619a5557bf4714a2d3900b25c6ef72c3bdf55
remco van der Velde <remco.pc@outlook.com>
2025-05-02 22:54:16 +0200

    implementing new parser

--- src/Package/Data/Application.sql
+++ src/Package/Data/Application.sql
@@ -1,2 +1,2 @@
 INSERT INTO "application" ("id", "uuid", "name", "url", "icon_url") VALUES
-    (1, '11fb658d-eef6-421f-867c-81ca7d2cd18d', 'Audio Player', '{{route.get(\'application-audio-player\')}}', '{{route.get(\'application-audio-player-icon\')}}');
+    (1, '11fb658d-eef6-421f-867c-81ca7d2cd18d', 'Audio Player', 'test', 'test2');
