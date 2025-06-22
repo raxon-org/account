9b0433bdd29e854c8242c4b56a4286f984dfffd8
remco van der Velde <remco.pc@outlook.com>
2025-06-22 21:47:18 +0200

    php 8.4 deprecations

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -298,7 +298,7 @@ class Permission
                 }
             } else {
                 $user_roles = [];
-                $roles = $user->getRole();
+                $roles = $user->role();
                 $has_permission = false;
                 $has_role = false;
                 $role = false;
