1821d686aeca3b384c03a5eb2abcd05ba38f3387
remco van der Velde <remco.pc@outlook.com>
2025-06-22 21:03:15 +0200

    php 8.4 deprecations

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -15,6 +15,7 @@ use Raxon\Module\Controller;
 use Raxon\Module\Database;
 use Raxon\Module\File;
 use Raxon\Module\Parse;
+use Raxon\Module\Validate;
 use Raxon\Node\Module\Node;
 
 class Permission
@@ -505,5 +506,98 @@ class Permission
             $object->request($attribute, $value);
         }
         return $request;
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws FileWriteException
+     * @throws Exception
+     */
+    protected static function validate(App $object, $url, $type){
+        $data = $object->data(sha1($url));
+        if($data === null){
+            $data = $object->parse_read($url, sha1($url));
+        }
+        if($data){
+            $validate = $data->data($type . '.validate');
+            if(empty($validate)){
+                return false;
+            }
+            return Validate::validate($object, $validate);
+        }
+        return false;
+    }
+
+    /**
+     * @throws ObjectException
+     */
+    protected static function castValue($array=[]): mixed
+    {
+        if(is_array($array)){
+            foreach($array as $key => $value) {
+                if(is_object($value) || is_array($value)){
+                    $array[$key] = Permission::castValue($value);
+                } else {
+                    if($value === 'null'){
+                        $array[$key] = null;
+                    }
+                    elseif($value === 'true'){
+                        $array[$key] = true;
+                    }
+                    elseif($value === 'false'){
+                        $array[$key] = false;
+                    }
+                    elseif(is_numeric($value)){
+                        $array[$key] = $value + 0;
+                    }
+                    elseif(substr($value, 0, 1) === '[' && substr($value, -1, 1) === ']'){
+                        $array[$key] = Core::object($value, Core::OBJECT_ARRAY);
+                    }
+                }
+            }
+            return $array;
+        }
+        elseif(is_object($array)){
+            foreach($array as $key => $value) {
+                if(is_object($value) || is_array($value)){
+                    $array->$key = Permission::castValue($value);
+                } else {
+                    if($value === 'null'){
+                        $array->$key = null;
+                    }
+                    elseif($value === 'true'){
+                        $array->$key = true;
+                    }
+                    elseif($value === 'false'){
+                        $array->$key = false;
+                    }
+                    elseif(is_numeric($value)){
+                        $array->$key = $value + 0;
+                    }
+                    elseif(substr($value, 0, 1) === '[' && substr($value, -1, 1) === ']'){
+                        $array->$key = Core::object($value, Core::OBJECT_ARRAY);
+                    }
+                }
+            }
+            return $array;
+        }
+        elseif($array === 'null'){
+            return null;
+        }
+        elseif($array === 'true'){
+            return true;
+        }
+        elseif($array === 'false'){
+            return false;
+        }
+        elseif(is_numeric($array)){
+            return $array + 0;
+        }
+        elseif(substr($array, 0, 1) === '[' && substr($array, -1, 1) === ']'){
+            return Core::object($array, Core::OBJECT_ARRAY);
+        }
+        else {
+            return $array;
+        }
     }
 }
