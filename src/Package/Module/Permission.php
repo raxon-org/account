54c7b5493604b62341b865f0e1fc136e2c05e7b4
remco van der Velde <remco.pc@outlook.com>
2025-06-22 22:05:16 +0200

    php 8.4 deprecations

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -147,10 +147,6 @@ class Permission
             $user = User::get_by_key($object);
             d($user);
             if(!$user){
-                $response = User::current($object);
-                ddd($response);
-
-
                 $user = User::get_by_authorization($object);
                 ddd($user);
             }
