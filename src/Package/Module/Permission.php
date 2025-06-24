0d1173093b565805940855265fa457b290133052
remco van der Velde <remco.pc@outlook.com>
2025-06-24 15:42:34 +0200

    php 8.4 deprecations

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -151,7 +151,7 @@ class Permission
             if(!$user){
                 $user = User::get_by_authorization($object);
             }
-            $user = User::expose($object, $user, $action);
+            $user = User::expose($object, $user, 'current');
             ddd($user);
             foreach($user->role() as $role){
                 if(!property_exists($role, 'permission')){
