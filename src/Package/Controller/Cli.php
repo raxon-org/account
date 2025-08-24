52444d3d6ac76adc5dbd2ec8930a27311aacb6fd
remco van der Velde <remco.pc@outlook.com>
2025-08-24 11:08:53 +0200

    cleanup

--- src/Package/Controller/Cli.php
+++ src/Package/Controller/Cli.php
@@ -22,8 +22,7 @@ class Cli extends Controller {
     const DIR = __DIR__ . '/';
     const MODULE_INFO = 'Info';
     const INFO = [
-        '{{binary()}} raxon/account                 | Account options',
-        '{{binary()}} raxon/account setup           | Setup Accounts',
+        '{{binary()}} raxon/account                 | Account options',        
     ];
 
     /**
