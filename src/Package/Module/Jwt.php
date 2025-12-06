a736d64e43a34bbe5bcf7eb19c0ff119ef3b6d8a
Remco van der Velde <remco@universeorange.com>
2025-12-06 13:48:18 +0100

    pp

--- src/Package/Module/Jwt.php
+++ src/Package/Module/Jwt.php
@@ -174,6 +174,7 @@ class Jwt {
         $options = [];
         $url = $object->config('project.dir.data') . 'Account/Jwt.json';
         $config  = $object->parse_read($url, sha1($url));
+        ddd($url);
         $configuration = Jwt::configuration($object, $options);        
         $token_unencrypted = $configuration->parser()->parse($token);        
         $clock = SystemClock::fromUTC(); // use the clock for issuing and validation
