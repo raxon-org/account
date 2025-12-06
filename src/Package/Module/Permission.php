11869420d732798a5b3dc155eb2d8de505d939ab
Remco van der Velde <remco@universeorange.com>
2025-12-06 17:17:14 +0100

    pp

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -154,7 +154,7 @@ class Permission
                 $user = User::expose($object, $user, 'current');
                 ddd($user);
             }
-            ddd('endtest');
+            ddd($user);
             $user = User::expose($object, $user, 'current');
             $roles = $user->role ?? [];
             foreach($roles as $role){
