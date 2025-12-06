7279251d9a72eb51646c6cead85f595e36f1264a
Remco van der Velde <remco@universeorange.com>
2025-12-06 17:32:59 +0100

    pp

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -144,6 +144,11 @@ class User
         );
         $node = $record;
         $record = [];
+        d($node);
+        d($expose);
+        d($entity);
+        d($function);
+        ddd($role);
         $record = \Raxon\Doctrine\Module\Entity::output(
             $object,
             $node,
