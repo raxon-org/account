4cf11dada642a9854656f93b1710e6bfd5d6f1d9
Remco van der Velde <remco@universeorange.com>
2025-12-06 17:20:38 +0100

    pp

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -179,6 +179,7 @@ class Permission
                 }
             }
         } catch (Exception $exception){
+            d($exception);
             if(!$user){
                 $class = 'Account.Role';
                 $node = new Node($object);
