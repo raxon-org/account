c17ca4a7ae3de293a44f89a7abad38b8eeefb178
Remco van der Velde <remco@universeorange.com>
2025-12-24 04:15:00 +0100

    pp

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -148,7 +148,7 @@ class Permission
         $has_permission = false;
         try {
             $user = User::get_by_key($object);
-            ddd($user);
+            ddd($object->get('key'));
             if(!$user){
                 $user = User::get_by_authorization($object);
             }
