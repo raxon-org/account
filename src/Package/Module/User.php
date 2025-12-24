94446a970d9f60dae5d1a854939da0664ef14f2c
Remco van der Velde <remco@universeorange.com>
2025-12-24 14:11:07 +0100

    pp

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -505,7 +505,6 @@ class User
     public static function get_by_authorization(App $object): mixed
     {
         $item = $object->config('user');
-        $item = null; //@2025-12-06 bug found, no roles and might be wrong role id, we don't get relations...
         if($item){
             return $item;
         }
