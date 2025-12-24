4bf3630de25fd4f1e76ab4f4468c6c6fdef24ef6
Remco van der Velde <remco@universeorange.com>
2025-12-24 13:45:33 +0100

    pp

--- src/Package/Module/Permission.php
+++ src/Package/Module/Permission.php
@@ -213,6 +213,7 @@ class Permission
         if($has_permission && $has_role){
             return $has_role;
         }
+        ddd('endtest');
         throw new AuthorizationException('You don\'t have permission to access this resource. (' . $controller . ':' . $action . ')');
     }
 
