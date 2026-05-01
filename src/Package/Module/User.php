a6d2874a7a0b1e36504e3c2e5e787926dcaf6968
Remco van der Velde <remco@universeorange.com>
2026-05-02 00:42:43 +0200

    ` init`

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -53,6 +53,7 @@ class User
             throw new ErrorException('Connection not configured.');
         }
         $connection->manager = Database::entity_manager($object, $config, $connection);
+        ddd('endtest');
         if(User::is_blocked($object, $input, $connection) === false){
             $repository = $connection->manager->getRepository(Entity::class);
             $node = $repository->findOneBy([
