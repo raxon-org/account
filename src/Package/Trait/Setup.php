a43a9f09d3ee14af4c9527c6de9fb92bf33f64a8
root <root@localhost>
2026-06-20 12:38:48 +0000

    init

--- src/Package/Trait/Setup.php
+++ src/Package/Trait/Setup.php
@@ -595,7 +595,8 @@ trait Setup {
             $options_jwt->refresh->token->issued_by = 'raxon.org';
         } else {
             $options_jwt->refresh->token->issued_by = $options->refresh->token->issued_by;
-        }    
+        }
+        // add $options_jwt->token->crypt_url = '{{config(\'project.dir.data\')}}Ssl/Defuse.key';
         $bytes = File::write($url_jwt, Core::object($options_jwt, Core::OBJECT_JSON));
         echo 'Written: ' . $url_jwt . ' size:  ' . File::size_format($bytes) . PHP_EOL;
         return true;
