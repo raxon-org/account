676543de34319fd9eefe62389f4a7f1e62e16bf5
Remco van der Velde <remco@universeorange.com>
2026-07-15 21:42:38 +0200

    init

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -607,9 +607,11 @@ class User
             //if you want you can logout everyone from the system by changing the content of crypt_url
             $key = Core::key($crypt_url);
             $token = base64_decode($token); // around 5900 still doesn't fit in the 4KB cookie so its in localstorage which should be subdomain level specific and around 5 MB
-            $token = gzdecode($token);
-            for($i = 0; $i < 3; $i++){
-                $token = Crypto::decrypt($token, $key); //around: 7650 chars doesn't fit in the 4KB cookie
+            if(strlen($token) >= 1){
+                $token = gzdecode($token);
+                for($i = 0; $i < 3; $i++){
+                    $token = Crypto::decrypt($token, $key); //around: 7650 chars doesn't fit in the 4KB cookie
+                }
             }
         }
         if(!$token){
