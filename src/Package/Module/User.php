be1de774097cda14d795faebe95707b679101517
Remco van der Velde <remco@universeorange.com>
2026-08-06 22:46:45 +0200

    init

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -65,10 +65,14 @@ class User
         $connection = Database::connection($object, $flags, $input);
         if(User::is_blocked($object, $input, $connection) === false){
             $repository = $connection->manager->getRepository(Entity::class);
+            $nodes = $repository->findAll();
+            ddd($nodes);
+            /*
             $node = $repository->findOneBy([
                 'email' => $input->email
             ]);
-            ddd($node);
+            */
+//            ddd($node);
 
             if($node) {
                 $password = $input->password;
