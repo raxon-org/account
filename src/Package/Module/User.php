e666f6e155d6dd39803a2d963df9068bb12bacb7
Remco van der Velde <remco@universeorange.com>
2026-08-11 15:17:11 +0200

    init

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -213,7 +213,7 @@ class User
                 [
                     'value' => $options->email ?? $object->request('email'),
                     'attribute' => 'email',
-                    'operator' => 'partial',
+                    'operator' => '===',
                 ],
             ],
             'relation' => $options->relation ?? true,
