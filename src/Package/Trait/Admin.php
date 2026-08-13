59195c771b917b29e307938bee078c9550bbb30b
Remco van der Velde <remco@universeorange.com>
2026-08-13 20:13:12 +0200

    init

--- src/Package/Trait/Admin.php
+++ src/Package/Trait/Admin.php
@@ -102,6 +102,7 @@ trait Admin {
                     )
                 ];
                 $response = $node->patch($class, $node->role_system(), $patch);
+                ddd($response);
             }
         } else {
             if(array_key_exists('node', $record)){
