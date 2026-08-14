16025d513784aeaf9fa3a5f96c97a649b0175c59
Remco van der Velde <remco@universeorange.com>
2026-08-14 23:31:45 +0200

    init

--- src/Package/Module/User.php
+++ src/Package/Module/User.php
@@ -76,7 +76,6 @@ class User
             $user->ip = $input->ip ?? '0.0.0.0';
             $user->token = User::get_token($object, $user);
             $user->refresh_token = User::get_refresh_token($object, $user);
-            ddd(strlen($user->refresh_token));
             return (object) [
                 'node' => $user,
             ];
